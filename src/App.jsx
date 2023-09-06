38e06d0d341672537887d3a492d60b82f7deb8b0
BishoyOsama <66043532+BishoyOsama@users.noreply.github.com>
2023-09-06 17:48:29 +0300

    done with sidebar for now

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,12 @@ import {
   Editor,
   Line,
 } from "./pages";
+import { useStateContext } from "./contexts/ContextProvider";
 
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
   return (
     <>
       <BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
+import { registerLicense } from "@syncfusion/ej2/base.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const licenseKey =
+  "ORg4AjUWIQA/Gnt2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5adkNjUHxWcnVVR2Be";
+registerLicense(licenseKey);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>
+);
